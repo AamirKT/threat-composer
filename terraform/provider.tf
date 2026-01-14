126a62916236b64cd23b34b30786a9dcaa5cdcf9
AamirKT <aamirt@hotmail.co.uk>
2026-01-14 01:32:06 +0000

    add s3 as remote backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,8 +5,15 @@ terraform {
       version = "6.27.0"
     }
   }
-}
 
+backend "s3" {
+    bucket = "threatcomposer-aamir-s3"
+    key    = "global/s3/terraform.tfstate"
+    region = "eu-west-2"
+    use_lockfile = true
+    encrypt = true
+  } 
+}
 provider "aws" {
   # Configuration options
   region = var.aws_region
